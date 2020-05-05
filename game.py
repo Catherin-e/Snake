590690b14ed79459fd1e81a4074e1860ae240a24
Cathy <43960435+Catherin-e@users.noreply.github.com>
2020-05-05 20:52:05 +0300

    Create step1
    1. Set up our 3 main classes - Snake, Apple, and Game
    2. Add height and weight to board

---
+++ game.py
@@ -0,0 +1,14 @@
+class Game(): 
+  def __init__(self, height, width):
+    self.height = height
+    self.width = width
+  
+
+  def render(self):
+    print ("Height: ", + self.height)
+    print ("Width:  ", + self.width)
+  
+
+parameters_of_board = Game(10,20)
+
+parameters_of_board.render()
